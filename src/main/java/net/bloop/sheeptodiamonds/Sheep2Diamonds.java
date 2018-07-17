e6b63d3f06c844dd2e2a1bc68e4f20d499f1703f
bloop428 <justinschrier@gmail.com>
2018-07-16 19:16:09 -0700

    HAHA

---
+++ src/main/java/net/bloop/sheeptodiamonds/Sheep2Diamonds.java
@@ -0,0 +1,25 @@
+package net.bloop.sheeptodiamonds;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Sheep2Diamonds.MODID, name = Sheep2Diamonds.NAME, version = Sheep2Diamonds.VERSION)
+public class Sheep2Diamonds
+{
+    public static final String MODID = "sheep2diamonds";
+    public static final String NAME = "Sheep 2 Diamonds";
+    public static final String VERSION = "1.0";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(new SheepHit());
+    }
+}
---
+++ src/main/java/net/bloop/sheeptodiamonds/SheepHit.java
@@ -0,0 +1,31 @@
+package net.bloop.sheeptodiamonds;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+import java.util.logging.Logger;
+
+public class SheepHit {
+
+    @SubscribeEvent
+    public void hitSheep(LivingDamageEvent event)
+    {
+        Entity target = event.getEntity();
+        World world = target.getEntityWorld();
+
+        //!world.isRemote MEANS SERVER SIDE ONLY
+        if(target instanceof EntitySheep && !world.isRemote)
+        {
+            world.spawnEntity(new EntityItem(world, target.posX, target.posY, target.posZ, new ItemStack(Items.DIAMOND)));
+        }
+    }
+}
